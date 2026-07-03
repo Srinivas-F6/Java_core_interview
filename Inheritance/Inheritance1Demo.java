33a7037ba773273bf5e665b47fe03d9dd5b10d69
_Srinivas_ <glsrinivas1819@gmail.com>
2026-07-03 15:08:44 +0530

    s

---
+++ Inheritance/Inheritance1Demo.java
@@ -0,0 +1,25 @@
+class Banking{
+    String name;
+    public Banking(){
+        System.out.println("Bank Constructor");
+    }
+}
+
+class SBI  extends Banking{
+    SBI(){
+        System.out.println("SBI constructor");
+    }
+    public void bankName(String name){
+        this.name = name;
+    }
+    public String bankName(){
+        return name;
+    }
+}
+public class Inheritance1Demo {
+    public static void main(String[] args) {
+        SBI sb = new SBI();
+        sb.bankName("sbi");
+        System.out.println(sb.bankName());
+    }   
+}
--- Inheritance/Single.java
+++ Inheritance/Single.java
@@ -1,83 +1,26 @@
-// class Calculator{
-//     public int add(int a, int b){
-//         return a+b;
-//     }
-//     public int sub(int a, int b){
-//         return a-b;
-//     }
-// }
+class Calculator{
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+}
 
-// class AdvCalculator extends Calculator{
-//     public int mul(int a, int b){
-//         return a*b;
-//     }
-//     public int div(int c, int d){
-//         return c/d;
-//     }
-// }
+class AdvCalculator extends Calculator{
+    public int mul(int a, int b){
+        return a*b;
+    }
+    public int div(int c, int d){
+        return c/d;
+    }
+}
 
-// public class Single{
-//     public static void main(String[] args) {
-//         AdvCalculator c = new AdvCalculator();
-//         System.out.print(c.add(5,7));
-//         System.out.print(c.mul(5,7));
+public class Single{
+    public static void main(String[] args) {
+        AdvCalculator c = new AdvCalculator();
+        System.out.println(c.add(5,7));
+        System.out.println(c.mul(5,7));
 
-//     }
-// }
-
-
-
-
-// Parent class
-// class BankAccount {
-//     protected double balance;
-
-//     public BankAccount(double initialBalance) {
-//         this.balance = initialBalance;
-//     }
-
-//     public void deposit(double amount) {
-//         balance += amount;
-//         System.out.println("Deposited: " + amount);
-//     }
-
-//     public void withdraw(double amount) {
-//         if(amount <= balance) {
-//             balance -= amount;
-//             System.out.println("Withdrawn: " + amount);
-//         } else {
-//             System.out.println("Insufficient balance!");
-//         }
-//     }
-
-//     public void showBalance() {
-//         System.out.println("Balance: " + balance);
-//     }
-// }
-
-
-// class SavingsAccount extends BankAccount {
-//     private double interestRate;
-//     public SavingsAccount(double initialBalance, double interestRate) {
-//         super(initialBalance); // call parent constructor...........super() is used for calling the parent class constructor
-//         this.interestRate = interestRate;
-//     }
-
-//     public void addInterest() {
-//         double interest = balance * interestRate / 100;
-//         deposit(interest); // reusing parent method
-//         System.out.println("Interest Added: " + interest);
-//     }
-// }
-
-// public class BankSystem {
-//     public static void main(String[] args) {
-//         SavingsAccount acc = new SavingsAccount(1000, 5);
-
-//         acc.showBalance();   // from parent
-//         acc.deposit(500);    // from parent
-//         acc.withdraw(200);   // from parent
-//         acc.addInterest();   // from child
-//         acc.showBalance();   // final balance
-//     }
-// }
+    }
+}
